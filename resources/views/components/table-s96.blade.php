24b017cb0a745af3cc26a93cda5aafbce801e7ce
Luis Miguel <luibelzan@outlook.com>
2025-02-20 10:31:07 +0100

    S96 and S96 table components created and displayed

---
+++ resources/views/components/table-s96.blade.php
@@ -0,0 +1,247 @@
+<div class="rgb(27,32,38) p-4 rounded-lg shadow-xl"
+    style="max-height: 400px; overflow-y: auto; scrollbar-width: thin; scrollbar-color: #888 rgb(27,32,38);">
+    <div class="mb-4"
+        style="border-bottom: 3px solid transparent;
+                border-image: linear-gradient(to right, rgb(27,32,38), rgb(42,50,62),rgb(27,32,38)) 1;">
+    </div>
+    <table id="tabla-eventos" class="w-full text-white text-center"
+        style="border-spacing: 0 5px;">
+        <thead style="border-bottom: 1px solid #ffffff;">
+            <tr>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">RTU ID</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">FECHA</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H1</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H2</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H3</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H4</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H5</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H6</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H7</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H8</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H9</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H10</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H11</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H12</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H13</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H14</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H15</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H16</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H17</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H18</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H19</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H20</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H21</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H22</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H23</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H24</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_H25</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_THD</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HR_BC</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H1</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H2</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H3</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H4</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H5</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H6</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H7</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H8</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H9</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H10</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H11</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H12</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H13</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H14</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H15</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H16</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H17</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H18</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H19</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H20</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H21</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H22</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H23</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H24</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_H25</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_THD</th>
+                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">HS_BC</th>
+
+
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($resultados as $resultado)
+            <tr>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->rtu_id) ? $resultado->rtu_id : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->fh) ? $resultado->fh : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h1) ? $resultado->hr_h1 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h2) ? $resultado->hr_h2 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h3) ? $resultado->hr_h3 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h4) ? $resultado->hr_h4 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h5) ? $resultado->hr_h5 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h6) ? $resultado->hr_h6 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h7) ? $resultado->hr_h7 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h8) ? $resultado->hr_h8 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h9) ? $resultado->hr_h9 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h10) ? $resultado->hr_h10 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h11) ? $resultado->hr_h11 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h12) ? $resultado->hr_h12 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h13) ? $resultado->hr_h13 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h14) ? $resultado->hr_h14 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h15) ? $resultado->hr_h15 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h16) ? $resultado->hr_h16 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h17) ? $resultado->hr_h17 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h18) ? $resultado->hr_h18 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h19) ? $resultado->hr_h19 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h20) ? $resultado->hr_h20 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h21) ? $resultado->hr_h21 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h22) ? $resultado->hr_h22 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h23) ? $resultado->hr_h23 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h24) ? $resultado->hr_h24 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_h25) ? $resultado->hr_h25 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_thd) ? $resultado->hr_thd : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hr_bc) ? $resultado->hr_bc : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h1) ? $resultado->hs_h1 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h2) ? $resultado->hs_h2 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h3) ? $resultado->hs_h3 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h4) ? $resultado->hs_h4 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h5) ? $resultado->hs_h5 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h6) ? $resultado->hs_h6 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h7) ? $resultado->hs_h7 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h8) ? $resultado->hs_h8 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h9) ? $resultado->hs_h9 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h10) ? $resultado->hs_h10 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h11) ? $resultado->hs_h11 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h12) ? $resultado->hs_h12 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h13) ? $resultado->hs_h13 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h14) ? $resultado->hs_h14 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h15) ? $resultado->hs_h15 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h16) ? $resultado->hs_h16 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h17) ? $resultado->hs_h17 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h18) ? $resultado->hs_h18 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h19) ? $resultado->hs_h19 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h20) ? $resultado->hs_h20 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h21) ? $resultado->hs_h21 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h22) ? $resultado->hs_h22 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h23) ? $resultado->hs_h23 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h24) ? $resultado->hs_h24 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_h25) ? $resultado->hs_h25 : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_thd) ? $resultado->hs_thd : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->hs_bc) ? $resultado->hs_bc : 'No hay datos' }}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
