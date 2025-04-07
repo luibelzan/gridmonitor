587986e29650965ed8dd433914b7e03d8aaa25db
Luis Miguel <luibelzan@outlook.com>
2025-04-07 10:06:00 +0200

    Pagination added to CT Events

--- resources/views/ct/eventosct.blade.php
+++ resources/views/ct/eventosct.blade.php
@@ -513,6 +513,11 @@
                                                                     </tbody>
                                                                 </table>
                                                             </div>
+                                                            <div class="pagination-container mt-4 flex justify-center items-center">
+                                                                <div class="pagination">
+                                                                    {{ $resultadosQ23->links() }}
+                                                                </div>
+                                                            </div>
                                                         @else
                                                             <div class="rgb(27,32,38) p-4 rounded-lg shadow-xl">
                                                                 <p class="mt-0 text-xl  text-center"
