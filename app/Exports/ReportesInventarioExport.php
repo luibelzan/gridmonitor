f03da5dbf34105f46e2d3d812c6a860f12b4dbe0
Luis Miguel <luibelzan@outlook.com>
2025-04-09 14:43:10 +0200

    Inventory reports paginated and exported to excel

---
+++ app/Exports/ReportesInventarioExport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class ReportesInventarioExport implements FromCollection, WithHeadings
+{
+    protected $data;
+
+    // Constructor para recibir los datos de la consulta
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    // Función que retorna los datos a exportar
+    public function collection()
+    {
+        return collect($this->data); // Convierte los datos en una colección
+    }
+
+    // Cabeceras de las columnas
+    public function headings(): array
+    {
+        return [
+            'Contador',
+            'Modelo',
+            'DLMS',
+            'Prime',
+            'Fabricante',
+            'Anio',
+            'Tipo',
+            'Companion',
+            'Nombre Fabricante',
+            'Fases',
+            'Tipo',
+        ];
+    }
+}
