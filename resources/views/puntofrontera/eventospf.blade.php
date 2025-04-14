b4956c988c77e6857ca41deee1d39263d0581e5f
Luis Miguel <luibelzan@outlook.com>
2025-04-14 15:26:10 +0200

    PF events exported to CSV

--- resources/views/puntofrontera/eventospf.blade.php
+++ resources/views/puntofrontera/eventospf.blade.php
@@ -210,42 +210,53 @@
         }
     </style>
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-        var exportButton = document.getElementById('exportarExcel');
-        
-        if (exportButton) {
-            exportButton.addEventListener('click', function () {
-                // Obtener los valores de los filtros
-                var descripcion = document.querySelector('input[name="descripcion"]').value;
-                var fecInicio = document.querySelector('input[name="fecha_inicio"]').value;
-                var fecFin = document.querySelector('input[name="fecha_fin"]').value;
-                var id_cnt = document.querySelector('input[name="id_cnt"]').value;
+document.addEventListener("DOMContentLoaded", function () {
 
-                // Construir la URL con todos los parámetros
-                var url = "{{ route('exportar.eventos.pf') }}?";
+    function exportarArchivo(formato) {
+        var descripcion = document.querySelector('input[name="descripcion"]').value;
+        var fecInicio = document.querySelector('input[name="fecha_inicio"]').value;
+        var fecFin = document.querySelector('input[name="fecha_fin"]').value;
+        var id_cnt = document.querySelector('input[name="id_cnt"]').value;
 
-                // Añadir los parámetros solo si están presentes
-                if (id_cnt) {
-                    url += "id_cnt=" + encodeURIComponent(id_cnt) + "&";
-                }
-                if (descripcion) {
-                    url += "descripcion=" + encodeURIComponent(descripcion) + "&";
-                }
-                if (fecInicio) {
-                    url += "fecha_inicio=" + encodeURIComponent(fecInicio) + "&";
-                }
-                if (fecFin) {
-                    url += "fecha_fin=" + encodeURIComponent(fecFin);
-                }
+        var url = "{{ route('exportar.eventos.pf') }}?";
 
-                // Redirigir al servidor con la URL construida
-                window.location.href = url;
-            });
-        } else {
-            console.error("El botón exportarExcel no existe en el DOM.");
+        if (id_cnt) {
+            url += "id_cnt=" + encodeURIComponent(id_cnt) + "&";
         }
-    });
-    </script>
+        if (descripcion) {
+            url += "descripcion=" + encodeURIComponent(descripcion) + "&";
+        }
+        if (fecInicio) {
+            url += "fecha_inicio=" + encodeURIComponent(fecInicio) + "&";
+        }
+        if (fecFin) {
+            url += "fecha_fin=" + encodeURIComponent(fecFin) + "&";
+        }
+
+        url += "format=" + formato;
+
+        window.location.href = url;
+    }
+
+    var exportExcelBtn = document.getElementById('exportarExcel');
+    var exportCsvBtn = document.getElementById('exportarCsv');
+
+    if (exportExcelBtn) {
+        exportExcelBtn.addEventListener('click', function () {
+            exportarArchivo('excel');
+        });
+    } else {
+        console.error("El botón exportarExcel no existe en el DOM.");
+    }
+
+    if (exportCsvBtn) {
+        exportCsvBtn.addEventListener('click', function () {
+            exportarArchivo('csv');
+        });
+    }
+});
+</script>
+
 
 
     <title>Eventos PF</title>
@@ -643,8 +654,17 @@
                                                             @endif
                                                             <!-- Contenedor del botón de descarga -->
                                                             <div class="text-right mt-4">
-                                                                <button id="exportarExcel" style="padding: 5px; border: none; border-radius: 5px; cursor: pointer; background-image: url('../../images/excel-icon.png'); background-size: cover; width: 30px; height: 30px;">
-                                                                   </button>
+                                                                <!-- Botón Excel -->
+                                                                <button id="exportarExcel" 
+                                                                    style="padding: 5px; border: none; border-radius: 5px; cursor: pointer; background-image: url('../../images/excel-icon.png'); background-size: cover; width: 30px; height: 30px;" 
+                                                                    title="Exportar a Excel">
+                                                                </button>
+
+                                                                <!-- Botón CSV -->
+                                                                <button id="exportarCsv" 
+                                                                    style="padding: 5px; border: none; border-radius: 5px; cursor: pointer; background-image: url('../../images/csv-icon.png'); background-size: cover; width: 30px; height: 30px;" 
+                                                                    title="Exportar a CSV">
+                                                                </button>
                                                             </div>
                                                             {{-- </div> --}}
                                                         </div>
