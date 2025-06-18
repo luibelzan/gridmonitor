83effab046744ac9cb7bd0aa837743a50188735a
Luis Miguel <luibelzan@outlook.com>
2025-06-18 14:11:28 +0200

    G53 table fixed

--- resources/views/components/table-g53.blade.php
+++ resources/views/components/table-g53.blade.php
@@ -4,141 +4,64 @@
         style="border-bottom: 3px solid transparent;
                 border-image: linear-gradient(to right, rgb(27,32,38), rgb(42,50,62),rgb(27,32,38)) 1;">
     </div>
-    <table id="tabla-eventos" class="w-full text-white text-center"
-        style="border-spacing: 0 5px;">
-        <thead style="border-bottom: 1px solid #ffffff;">
-            <tr>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">RTU ID</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">LVS ID</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">LVS POS</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">FECHA</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMCPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMVPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPIMPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPEXPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQIMPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQEXPH1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPF1</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMCPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMVPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPIMPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPEXPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQIMPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQEXPH2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPF2</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMCPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMVPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPIMPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPEXPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQIMPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMQEXPH3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMPF3</th>
-                <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">MOMCN</th>
+    <table id="tabla-g53" class="w-full text-white text-center" style="border-spacing: 0 5px;">
+    <thead style="border-bottom: 1px solid #ffffff;">
+        <tr>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">RTU ID</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">FECHA</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">VTN MIN</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">VTN AVG</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">VTN MAX</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">VTN LSM</th>
+            <th class="text-xl font-bold text-center" style="color:rgb(88,226,194); padding: 10px;">BC</th>
+        </tr>
+    </thead>
 
-            </tr>
-        </thead>
-
-        @php
-            $hayDatos = false;
-            foreach ($resultados as $r) {
-                if (!empty($r->rtu_id) || !empty($r->lvs_id) || !empty($r->fh)) {
-                    $hayDatos = true;
-                    break;
-                }
+    @php
+        $hayDatos = false;
+        foreach ($resultados as $r) {
+            if (!empty($r->rtu_id) || !empty($r->fh)) {
+                $hayDatos = true;
+                break;
             }
-        @endphp
+        }
+    @endphp
 
-        <tbody>
-            @if(!$hayDatos)
-                <tr>
-                    <td colspan="26" class="py-4 text-center text-gray-400">
-                        No hay datos disponibles
-                    </td>
-                </tr>
-            @else
-                @foreach($resultados as $resultado)
-                <tr>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->rtu_id) ? $resultado->rtu_id : 'No hay datos' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->lvs_id) ? $resultado->lvs_id : 'No hay datos' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->lvs_pos) ? $resultado->lvs_pos : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->fh) ? $resultado->fh : 'No hay datos' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momCph1) ? $resultado->momCph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momVph1) ? $resultado->momVph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPimph1) ? $resultado->momPimph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->MomPexph1) ? $resultado->MomPexph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQimph1) ? $resultado->momQimph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQexph1) ? $resultado->momQexph1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPF1) ? $resultado->momPF1 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momCph2) ? $resultado->momCph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momVph2) ? $resultado->momVph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPimph2) ? $resultado->momPimph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPexph2) ? $resultado->momPexph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQimph2) ? $resultado->momQimph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQexph2) ? $resultado->momQexph2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPF2) ? $resultado->momPF2 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momCph3) ? $resultado->momCph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momVph3) ? $resultado->momVph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPimph3) ? $resultado->momPimph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPexph3) ? $resultado->momPexph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQimph3) ? $resultado->momQimph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momQexph3) ? $resultado->momQexph3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momPF3) ? $resultado->momPF3 : '0' }}
-                    </td>
-                    <td class="py-2" style="padding: 10px;">
-                        {{ !empty($resultado->momCn) ? $resultado->momCn : '0' }}
-                    </td>
-                </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
+    <tbody>
+        @if(!$hayDatos)
+            <tr>
+                <td colspan="7" class="py-4 text-center text-gray-400">
+                    No hay datos disponibles
+                </td>
+            </tr>
+        @else
+            @foreach($resultados as $resultado)
+            <tr>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->rtu_id) ? $resultado->rtu_id : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->fh) ? $resultado->fh : 'No hay datos' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->vtnMin) ? $resultado->vtnMin : '0' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->vtnAvg) ? $resultado->vtnAvg : '0' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->vtnMax) ? $resultado->vtnMax : '0' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->vtnLsm) ? $resultado->vtnLsm : '0' }}
+                </td>
+                <td class="py-2" style="padding: 10px;">
+                    {{ !empty($resultado->bc) ? $resultado->bc : 'No hay datos' }}
+                </td>
+            </tr>
+            @endforeach
+        @endif
+    </tbody>
+</table>
+
 </div>
